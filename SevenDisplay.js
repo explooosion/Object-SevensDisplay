cb1c879a43a3d70234f158a2e22c03573277b055
Robby <ta7382@gmail.com>
2016-04-17 01:46:41 +0800

    new file:   SevenDisplay.css 	new file:   SevenDisplay.js 	new file:   demo.html

---
+++ SevenDisplay.js
@@ -0,0 +1,63 @@
+var sevenDisplay = new Object(); // setting
+
+var dp = new Object();
+dp = [
+	"1,1,1,1,1,1,0",
+	"0,1,1,0,0,0,0",
+	"1,1,0,1,1,0,1",
+	"1,1,1,1,0,0,1",
+	"0,1,1,0,0,1,1",
+	"1,0,1,1,0,1,1",
+	"1,0,1,1,1,1,1",
+	"1,1,1,0,0,0,0",
+	"1,1,1,1,1,1,1",
+	"1,1,1,1,0,1,1"
+];
+
+$(function() {
+
+	$('.sevenDisplay').sevenDisplay({
+			speed: "1000"
+		});
+
+});
+
+
+(function($) {
+	$.fn.extend({
+		sevenDisplay: function(array) {
+			sevenDisplay = array;
+			if (sevenDisplay.speed == undefined)
+			{
+				sevenDisplay.speed = 1000;
+			}
+			AutoPlay();
+
+		}
+	});
+})(jQuery);
+
+var counter = 0;
+
+function AutoPlay() {
+
+	var num = dp[counter].split(",");
+	num.forEach(function(value, index) {
+		index++;
+		if (value == 1) {
+			$('.sevenDisplay div:nth-child(' + index + ')').show();
+		} else {
+			$('.sevenDisplay div:nth-child(' + index + ')').hide();
+		}
+	});
+
+	if (counter == 9) {
+		counter = 0;
+	} else {
+		counter++;
+	}
+
+	setTimeout(AutoPlay, sevenDisplay.speed);
+
+
+}
